68325b706330adb0352efc5ddc6e858fb9852edd
Raj <raj@d2america.com>
2024-09-04 21:22:33 +0530

    Fix conditions to render AI Image generation logic

--- src/app/dashboard2/gallery/BuyCredits.tsx
+++ src/app/dashboard2/gallery/BuyCredits.tsx
@@ -1,35 +1,42 @@
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import axios from '@/service/axiosInstance';
+import { useState } from 'react';
 import { FaCoins } from "react-icons/fa6";
 
 
 const BuyCredits = () => {
+    const [quantity, setQuantity] = useState(1);
     const onClickCheckout = async () => {
-        const getPayment = await axios.post("product/buyCredits",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    // 'Content-Type': 'application/x-www-form-urlencoded',
-                },               
-            }
-        );
+        const getPayment = await axios.post("product/buyCredits", { quantity: quantity });
         // const getPaymentParse = await getPayment.json();
-        console.log(getPayment)
+        console.log("getPayment: ", getPayment)
         window.location.replace(getPayment.data.session.url);
     }
 
     return (
         <div className='flex flex-col items-center'>
-            <h2>You have 0 available credits.</h2>
-            <button
-                className='w-full text-center my-4 p-2.5 text-sm font-medium text-black rounded-lg bg-green-600 hover:bg-green-700  focus:ring-4 focus:outline-none focus:ring-green-400'
-                onClick={() => onClickCheckout()}>
-                <span className="flex items-center justify-center ml-2">
-                    Buy 30 credits
-                    <FaCoins className="ml-1" />
-                </span>
-            </button>
-        </div>
+            <h2 className='text-red-500'>You have 0 available credits.</h2>
+            <div className='my-4'>
+                <Label className='text-base'>
+                    <span className="flex items-center justify-center ml-2">
+                        Buy Packs of 30 credits
+                        <FaCoins className="ml-1" />
+                    </span>
+                </Label>
+                <div className='flex justify-center items-center gap-5 my-4'>
+                    <Label className='text-base'>Quantity</Label>
+                    <Input className="w-full" type="number" placeholder='1' id='quantity' onChange={(e) => setQuantity(Number(e.target.value))} />
+                </div>
+                <button
+                    className='w-full text-center  p-2.5 text-sm font-medium text-black rounded-lg bg-green-600 hover:bg-green-700  focus:ring-4 focus:outline-none focus:ring-green-400'
+                    onClick={() => onClickCheckout()}>
+                    <span className="flex items-center justify-center ml-2">
+                        Purchase
+                    </span>
+                </button>
+            </div>
+        </div >
     )
 }
 
--- src/app/dashboard2/gallery/page.tsx
+++ src/app/dashboard2/gallery/page.tsx
@@ -11,9 +11,10 @@ import getCroppedImg from "./cropImage";
 import BeatLoader from "react-spinners/BeatLoader";
 import GenerateAIImage from "./GenerateAIImage";
 import BuyCredits from "./BuyCredits";
+import SyncLoader from "react-spinners/SyncLoader";
 const Gallery = () => {
   const { mutate, isLoading: isLoadingArt } = useUploadArt();
-  const { data, isLoading, refetch } = useGetGallery();
+  const { data, isLoading: isLoadingGallery, refetch } = useGetGallery();
   const [imageName, setImageName] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
   const [crop, setCrop] = useState({ x: 0, y: 0 });
@@ -106,131 +107,141 @@ const Gallery = () => {
   }, [isLoadingArt]);
 
   console.log("selectedFile", fileRejections);
+  return isLoadingGallery ?
+    <div className="flex items-center justify-center h-screen"><SyncLoader /></div>
+    :
+    (
+      <PageLayout>
+        <PageTitle>Gallery</PageTitle>
 
-  return (
-    <PageLayout>
-      <PageTitle>Gallery</PageTitle>
-
-      <div className="grid md:grid-cols-1 lg:grid-cols-[1fr,300px] gap-12">
-        <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] gap-4 my-4">
-          {data &&
-            data.data.gallery.map((img) => {
-              return (
-                <div key={img.id}>
-                  <div className="w-[120px] h-[180px] relative bg-[#F8F9F9] grid grid-rows-[1fr_100px] border rounded-sm">
-                    <Image
-                      key={img.id}
-                      src={img.urlImage}
-                      layout="fill"
-                      objectFit="contain"
-                      alt={img.name}
-                    />
+        <div className="grid md:grid-cols-1 lg:grid-cols-[1fr,300px] gap-12">
+          <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] gap-4 my-4">
+            {data &&
+              data.data.gallery.map((img) => {
+                return (
+                  <div key={img.id}>
+                    <div className="w-[120px] h-[180px] relative bg-[#F8F9F9] grid grid-rows-[1fr_100px] border rounded-sm">
+                      <Image
+                        key={img.id}
+                        src={img.urlImage}
+                        layout="fill"
+                        objectFit="contain"
+                        alt={img.name}
+                      />
+                    </div>
+                    <h2 className="text-lg font-semibold mt-2">{img.name}</h2>
                   </div>
-                  <h2 className="text-lg font-semibold mt-2">{img.name}</h2>
-                </div>
-              );
-            })}
-        </div>
-        <div>
-          <button
-            className="w-full mb-4 p-2.5 text-sm font-medium text-white rounded-lg border bg-blue-600 hover:bg-blue-700  focus:ring-4 focus:outline-none focus:ring-blue-300"
-            onClick={toggleUseAIForImage}
-          >
-            {useAIForImage ? "Upload a custom image" : "Generate an Image using AI"}
-          </button>
-          <div className="grid gap-4 bg-gray-900 p-6 rounded-lg text-white">
-            {useAIForImage ? (
-              data?.data.credits > 0 ?
-              <BuyCredits/>
-              :
-              <GenerateAIImage onImageGenerated={handleAIGeneratedImage} availableCredits={data?.data.credits}/>
-            ) : (
-              <div>
-                <h3 className='text-white mb-2 text-center'>Upload Custom Image</h3>
-                <div className="flex flex-col gap-4">
+                );
+              })}
+          </div>
+          <div>
+            <button
+              className="w-full mb-4 p-2.5 text-sm font-medium text-white rounded-lg border bg-blue-600 hover:bg-blue-700  focus:ring-4 focus:outline-none focus:ring-blue-300"
+              onClick={toggleUseAIForImage}
+            >
+              {useAIForImage ? "Upload a custom image" : "Generate an Image using AI"}
+            </button>
+            <div className="grid gap-4 bg-gray-900 p-6 rounded-lg text-white">
+              {useAIForImage ? (
+                data?.data.credits > 0 ?
+                  <GenerateAIImage onImageGenerated={handleAIGeneratedImage} availableCredits={data?.data.credits} />
+                  :
+                  <BuyCredits />
+              ) : (
+                <div>
+                  <h3 className='text-white mb-2 text-center'>Upload Custom Image</h3>
+                  <div className="flex flex-col gap-4">
 
-                  <div>
-                    <label htmlFor="title" className="text-sm">Title</label>
-                    <input
-                      type="text"
-                      id="title"
-                      placeholder="Enter Art Title"
-                      value={imageName}
-                      onChange={(e) => setImageName(e.target.value)}
-                      className="p-2 w-full text-sm bg-gray-800 text-white rounded border border-gray-700"
-                    />
-                  </div>
+                    <div>
+                      <label htmlFor="title" className="text-sm">Title</label>
+                      <input
+                        type="text"
+                        id="title"
+                        placeholder="Enter Art Title"
+                        value={imageName}
+                        onChange={(e) => setImageName(e.target.value)}
+                        className="p-2 w-full text-sm bg-gray-800 text-white rounded border border-gray-700"
+                      />
+                    </div>
 
-                  <div
-                    {...getRootProps({ className: "dropzone" })}
-                    className="flex items-center justify-center p-2 bg-gray-700 rounded border border-gray-600 cursor-pointer"
-                  >
-                    <input {...getInputProps()} />
-                    {acceptedFiles.length > 0 ? (
-                      <div>{acceptedFiles[0].path}</div>
-                    ) : fileRejections.length > 0 &&
-                      fileRejections[0].errors[0].code === "file-too-large" ? (
-                      <div className="text-red-600 font-bold">
-                        File is larger than 30 Mb
+                    <div
+                      {...getRootProps({ className: "dropzone" })}
+                      className="flex items-center justify-center p-2 bg-gray-700 rounded border border-gray-600 cursor-pointer"
+                    >
+                      <input {...getInputProps()} />
+                      {acceptedFiles.length > 0 ? (
+                        <div>{acceptedFiles[0].path}</div>
+                      ) : fileRejections.length > 0 &&
+                        fileRejections[0].errors[0].code === "file-too-large" ? (
+                        <div className="text-red-600 font-bold">
+                          File is larger than 30 Mb
+                        </div>
+                      ) : (
+                        <div>Upload logo in PNG</div>
+                      )}
+                      <div className="ml-2 p-1 border border-gray-500 rounded-full">
+                        <IconUpload />
                       </div>
-                    ) : (
-                      <div>Upload logo in PNG</div>
-                    )}
-                    <div className="ml-2 p-1 border border-gray-500 rounded-full">
-                      <IconUpload />
                     </div>
                   </div>
                 </div>
-              </div>
-            )}
-            <div className="mt-4 space-y-2">
-              <div>
-                <input type="checkbox" id="shouldCreatePoster" name="shouldCreatePoster" checked={shouldCreatePoster} onChange={handlePosterChange} />
-                <label htmlFor="shouldCreatePoster" className="text-white ml-2">Create Poster</label>
-              </div>
-              <div>
-                <input type="checkbox" id="shouldCreateCanvas" name="shouldCreateCanvas" checked={shouldCreateCanvas} onChange={handleCanvasChange} />
-                <label htmlFor="shouldCreateCanvas" className="text-white ml-2">Create Canvas</label>
-              </div>
-            </div>
-            <div className="text-white">Aspect Ratio 2/3</div>
-            <div className="relative h-64">
-              {urlImage && (
-                <Cropper
-                  image={urlImage}
-                  crop={crop}
-                  zoom={zoom}
-                  aspect={2 / 3}
-                  onCropChange={setCrop}
-                  onCropComplete={onCropComplete}
-                  onZoomChange={setZoom}
-                />
               )}
+              {(data?.data.credits === 0 && useAIForImage) ?
+                <></> 
+                :
+                <>
+                  <div className="mt-4 space-y-2">
+                    <div>
+                      <input type="checkbox" id="shouldCreatePoster" name="shouldCreatePoster" checked={shouldCreatePoster} onChange={handlePosterChange} />
+                      <label htmlFor="shouldCreatePoster" className="text-white ml-2">Create Poster</label>
+                    </div>
+                    <div>
+                      <input type="checkbox" id="shouldCreateCanvas" name="shouldCreateCanvas" checked={shouldCreateCanvas} onChange={handleCanvasChange} />
+                      <label htmlFor="shouldCreateCanvas" className="text-white ml-2">Create Canvas</label>
+                    </div>
+                  </div>
+                  <div className="text-white">Aspect Ratio 2/3</div>
+                  <div className="relative h-64">
+                    {urlImage && (
+                      <Cropper
+                        image={urlImage}
+                        crop={crop}
+                        zoom={zoom}
+                        aspect={2 / 3}
+                        onCropChange={setCrop}
+                        onCropComplete={onCropComplete}
+                        onZoomChange={setZoom}
+                      />
+                    )}
+                  </div>
+                  <input
+                    type="range"
+                    value={zoom}
+                    min={1}
+                    max={3}
+                    step={0.1}
+                    onChange={(e) => setZoom(e.target.value)}
+                    className="w-full mt-2"
+                  />
+                  <button
+                    onClick={showCroppedImage}
+                    className="mt-4 p-2 text-sm font-medium text-white bg-blue-600 rounded border border-blue-600 hover:bg-blue-700"
+                  >
+                    {isLoadingArt ? (
+                      <BeatLoader loading={isLoadingArt} color="white" size={16} />
+                    ) : (
+                      "Upload logo"
+                    )}
+                  </button>
+                </>
+
+
+              }
             </div>
-            <input
-              type="range"
-              value={zoom}
-              min={1}
-              max={3}
-              step={0.1}
-              onChange={(e) => setZoom(e.target.value)}
-              className="w-full mt-2"
-            />
-            <button
-              onClick={showCroppedImage}
-              className="mt-4 p-2 text-sm font-medium text-white bg-blue-600 rounded border border-blue-600 hover:bg-blue-700"
-            >
-              {isLoadingArt ? (
-                <BeatLoader loading={isLoadingArt} color="white" size={16} />
-              ) : (
-                "Upload logo"
-              )}
-            </button>
           </div>
         </div>
-      </div>
-    </PageLayout>
-  );
+      </PageLayout>
+    );
 };
 
 export default Gallery;
